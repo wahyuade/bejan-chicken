2f20aaad23c45be05fdf93e4324c3c92a86069a0
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2017-12-27 23:20:44 +0700

    admin list pesanan

--- controller/AdminController.js
+++ controller/AdminController.js
@@ -48,5 +48,43 @@ module.exports = (Admin, express, db)=>{
         res.render('admin/index');
     });
 
+    /**
+     * API Admin
+     */
+
+    ApiAdmin.get('/harga', (req, res)=>{
+        db.query('SELECT * FROM harga', (err, harga)=>{
+            res.json(harga);
+        });
+    });
+
+    ApiAdmin.get('/kandang', (req, res)=>{
+        db.query('SELECT * FROM kandang', (err, kandang)=>{
+            res.json(kandang);
+        });
+    });
+
+    ApiAdmin.get('/pembeli', (req, res)=>{
+        db.query('SELECT * FROM users WHERE jenis="U"', (err, pembeli)=>{
+            res.json(pembeli);
+        });
+    });
+
+    ApiAdmin.get('/penjualan', (req, res)=>{
+        db.query('SELECT penjualan.id_penjualan, users.nama, penjualan.jumlah, penjualan.tgl_pesan, penjualan.tgl_sampai, penjualan.jenis, penjualan.status, penjualan.harga FROM penjualan LEFT JOIN users ON penjualan.id_pembeli=users.id_login', (err, pembeli)=>{
+            if(pembeli.length > 0){
+                res.json({success:true, message:'Berhasil mengambil data penjualan', data:pembeli});
+            }else{
+                res.json({success:false, message:'Gagal mengambil data'});
+            }
+        });
+    });
+
+    ApiAdmin.get('/profile', (req, res)=>{
+        db.query('SELECT * FROM users WHERE token=?', req.headers.cookie.slice(6), (err, admin)=>{
+            res.json(admin[0]);
+        })
+    });
+
     Admin.use('/api', ApiAdmin);
 }
